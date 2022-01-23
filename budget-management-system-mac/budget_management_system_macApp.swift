6cbb7e087f5b2781535477fe02a1d25293fa0bf2
Levi Gratico <levigratico@gmail.com>
2022-01-23 22:04:15 +0800

    Initial commit

---
+++ budget-management-system-mac/budget_management_system_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  budget_management_system_macApp.swift
+//  budget-management-system-mac
+//
+//  Created by Levi Gratico on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct budget_management_system_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ budget-management-system-macTests/budget_management_system_macTests.swift
@@ -0,0 +1,33 @@
+//
+//  budget_management_system_macTests.swift
+//  budget-management-system-macTests
+//
+//  Created by Levi Gratico on 1/23/22.
+//
+
+import XCTest
+@testable import budget_management_system_mac
+
+class budget_management_system_macTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
